e72b94968cdc20339aa2e63cc814d3db130ceddc
Andrew Shapovalov <ecamp3r@gmail.com>
2019-01-21 10:37:55 +0200

    inited eslint config

---
+++ client/main.js
@@ -0,0 +1,9 @@
+// Client entry point, imports all client code
+import { Meteor } from 'meteor/meteor';
+
+import '/imports/startup/client';
+// import '/imports/startup/both';
+
+Meteor.startup(() => {
+	// code to run on server at startup
+});
