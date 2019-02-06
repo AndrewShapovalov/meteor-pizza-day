ad5388e4d507932e46cfa9c92e878cef34610415
Andrew <ecamp3r@gmail.com>
2019-02-06 18:58:19 +0200

    added ServiceConfiguration

--- server/main.js
+++ server/main.js
@@ -1,1 +1,22 @@
 import "/imports/startup/server";
+
+const createServiceConfiguration = (service, clientId, secret) => {
+
+  ServiceConfiguration.configurations.remove({
+    service,
+  });
+
+  const config = {
+    service,
+    clientId,
+    secret,
+  };
+
+  ServiceConfiguration.configurations.insert(config);
+};
+
+if (Meteor.settings && Meteor.settings.private && Meteor.settings.private.oAuth) {
+  const { oAuth: { google } } = Meteor.settings.private;
+
+  createServiceConfiguration("google", google.clientId, google.secret);
+}
